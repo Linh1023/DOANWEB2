00eae02abe3378dcf7e432f19c354011db532756
Linh1023 <116327425+Linh1023@users.noreply.github.com>
2023-04-14 16:15:28 +0700

    add file detail.php

--- admin/template/content_ad.php
+++ admin/template/content_ad.php
@@ -1,24 +1,28 @@
 <div id="content"class="row" style="background-color:#f0f5f8;height:calc(100% - 72px)">
 <?php
-if(isset($_GET['id'])){
+if(isset($_GET['id'])&&isset($_GET['hd'])&&isset($_GET['idsp'])){
+    
+    include('template_content/detail.php');
+}
+elseif(isset($_GET['id'])){
     switch($_GET['id']){
         case 'tc':
-            include('template_main/trangchu.php');
+            include('template_content/trangchu.php');
         break;
         case 'nd':
-            include('template_main/nguoidung.php');
+            include('template_content/nguoidung.php');
         break;
         case 'sp':
-            include('template_main/sanpham.php');
+            include('template_content/sanpham.php');
         break;
         case 'dh':
-            include('template_main/donhang.php');
+            include('template_content/donhang.php');
         break;
         case 'bd':
-            include('template_main/baidang.php');
+            include('template_content/baidang.php');
         break;
         case 'pq':
-            include('template_main/phanquyen.php');
+            include('template_content/phanquyen.php');
         break;
     }
 }
