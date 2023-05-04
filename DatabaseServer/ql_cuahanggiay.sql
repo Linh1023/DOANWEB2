19492c304ce24e5ed33071e75c559184f533038d
trungkien2543 <trungkien1862@gmail.com>
2023-05-04 14:17:31 +0700

    update

--- DatabaseServer/ql_cuahanggiay.sql
+++ DatabaseServer/ql_cuahanggiay.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 04, 2023 at 09:15 AM
+-- Generation Time: May 04, 2023 at 09:17 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
