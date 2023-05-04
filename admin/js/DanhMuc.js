5798b94a9466e7e68ee6a2e6e3d7517041c20107
trungkien2543 <trungkien1862@gmail.com>
2023-05-04 11:03:34 +0700

    Update

---
+++ admin/js/DanhMuc.js
@@ -0,0 +1,39 @@
+function exit(){
+    let a = document.getElementById("form_dm");
+    a.style.display = "none";
+
+    let MaDanhMuc = document.getElementById("MaDanhMuc");
+    MaDanhMuc.value = "";
+
+    let TenDanhMuc = document.getElementById("TenDanhMuc");
+    TenDanhMuc.value = "";
+}
+
+function add(){
+    let form = document.getElementById("form_dm");
+    form.style.display = "block";
+
+    let a = document.getElementById("a_dm");
+    a.href = "./template/template_content/Hang/xulyHang.php?cn=Add";
+
+    let nd = document.getElementById("tt_dm");
+    nd.innerHTML = "Thêm thông tin danh mục";
+}
+
+function edit(MaDanhMuc,TenDanhMuc) { 
+    let input1 = document.getElementById("MaDanhMuc");
+    input1.readOnly = true;
+    input1.value = MaDanhMuc;
+
+    let input2 = document.getElementById("TenDanhMuc");
+    input2.value = TenDanhMuc;
+
+    let form = document.getElementById("form_dm");
+    form.style.display = "block";
+
+    let a = document.getElementById("a_dm");
+    a.href = "./template/template_content/Hang/xulyHang.php?cn=Edit";
+
+    let nd = document.getElementById("tt_dm");
+    nd.innerHTML = "Sửa thông tin danh mục";
+}
