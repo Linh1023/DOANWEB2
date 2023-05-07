850eee8adf7907dea2229ccb02cffb2aa238eac5
trungkien2543 <trungkien1862@gmail.com>
2023-05-07 16:50:09 +0700

    Update

---
+++ template/mainProduct.php
@@ -0,0 +1,96 @@
+<div id="main_product">
+    <div id = "top_main">
+        <div id = "selection">
+            <div class = "item_selection">
+                <label>
+                        <input type = "radio" name = "img_selected" />
+                        <img src = "./img/products/001.jpg">
+                </label>
+            </div>
+            <div class = "item_selection">
+                <label>
+                        <input type = "radio" name = "img_selected" />
+                        <img src = "./img/products/001.jpg">
+                </label>
+            </div>
+            <div class = "item_selection">
+                <label>
+                        <input type = "radio" name = "img_selected" />
+                        <img src = "./img/products/001.jpg">
+                </label>
+            </div>
+            <div class = "item_selection">
+                <label>
+                        <input type = "radio" name = "img_selected" />
+                        <img src = "./img/products/001.jpg">
+                </label>
+            </div>
+        </div>
+        <div id = "image">
+            <img src = "./img/products/001.jpg">
+        </div>
+        <div id = "info">
+            <h1>ADIDAS NEMEZIZ 19.3 TF TRẮNG XANH MUTATOR 2020 PACK</h2>
+            <div id = "price">
+                <p>1,000,000đ</p>
+            </div>
+            <p>Kích thước</p>
+            <div id = "size">
+                <ul id = "size_list">
+                    <li class = "size-item">
+                        <label>
+                            <input type = "radio" name = "option" value = "39">
+                            <span>39</span>
+                        </label>
+                    </li>
+                    <li class = "size-item">
+                        <label>
+                            <input type = "radio" name = "option" value = "40">
+                            <span>40</span>
+                        </label>
+                    </li>
+                    <li class = "size-item">
+                        <label>
+                            <input type = "radio" name = "option" value = "41">
+                            <span>41</span>
+                        </label>
+                    </li>
+                    <li class = "size-item">
+                        <label>
+                            <input type = "radio" name = "option" value = "42">
+                            <span>42</span>
+                        </label>
+                    </li>
+                    <li class = "size-item">
+                        <label>
+                            <input type = "radio" name = "option" value = "43">
+                            <span>43</span>
+                        </label>
+                    </li>
+                    <li class = "size-item">
+                        <label>
+                            <input type = "radio" name = "option" value = "44">
+                            <span>44</span>
+                        </label>
+                    </li>
+                </ul>
+            </div>
+            <div id = "tonkho">
+                <p>
+                    Tồn kho:
+                    <span>12</span>  
+                </p>
+            </div>
+            <div id = "giohang">
+                <a href = "#">
+                    <span id="icon"><i class="ti-shopping-cart"></i></span> 
+                    <span id = "themvaogio">Thêm vào giỏ</span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div id = "bottom_main">
+        
+
+    </div>
+</div>
