39d977298c38283646419f45e01d53ed97ec3c8b
Le Hoai Nam <namhoaile2003@gmail.com>
2023-05-11 10:32:19 +0700

    nam uopdate

--- account/register.php
+++ account/register.php
@@ -3,7 +3,7 @@
 <link rel="stylesheet" href="../css/formDN.css"/>
 <link rel="stylesheet" href="themify-icons-font/themify-icons/themify-icons.css">
 
-<form method="post" action="./index.php" id="formdk">
+<form method="post" action="" id="formdk">
     <p onclick=tatdk()>X</p>
     <h2>Đăng ký</h2>
     Username: 
@@ -12,11 +12,17 @@
     <input type="password" name="password" value="" required/>
     Email: 
     <input type="text" name="email" value="" required/>
+    Tên khách hàng:
+    <input type="text" name="TenKhach" value="" required/>
+    Địa chỉ:
+    <input type="text" name="DiaChi" value="" required/>
+    Số điện thoại:
+    <input type="text" name="SDT" value="" required/>
     <br/>
     <br/>
     <input type="button" name="dangnhap" value="Đăng Nhập" onclick="hienthidangnhap()"/>
     <br/>
     <br/>
     <input type="submit" name="dangky" value="Đăng Ký"/>
-    <?php require 'register_submit.php';?> 
+    <?php require 'register_submit.php';?>
 </form>
--- account/register_submit.php
+++ account/register_submit.php
@@ -3,12 +3,27 @@
 // Kết nối cơ sở dữ liệu
 $conn = mysqli_connect('localhost', 'root', '', 'ql_cuahanggiay') or die ('Lỗi kết nối'); mysqli_set_charset($conn, "utf8");
 
+function getMaTaiKhoan($conn){
+    $sql5 = 'SELECT MAX(MaTaiKhoan) FROM taikhoan';
+    $data = null;
+    if($result = mysqli_query($conn,$sql5)){
+        while($row = mysqli_fetch_array($result)){
+            $data[] = $row;
+        }
+        mysqli_free_result($result);
+        return $data[0];
+    }
+}
 
 // Dùng isset để kiểm tra Form
 if(isset($_POST['dangky'])){
     $username = trim($_POST['username']); //trim để loại bỏ khoảng trắng ở đầu và cuối của username
     $password = trim($_POST['password']);
     $email = trim($_POST['email']);
+    $TenKhach= trim($_POST['TenKhach']);
+    $DiaChi= trim($_POST['DiaChi']);
+    $SDT= trim($_POST['SDT']);
+
 
     if(!$username || !$password || !$email) {
         echo '<script language="javascript">alert("Vui lòng nhập đầy đủ thông tin!"); window.location="index.php";</script>';
@@ -19,10 +34,8 @@ if(isset($_POST['dangky'])){
 
     // Kiểm tra username hoặc email có bị trùng hay không
     $sql = "SELECT * FROM taikhoan WHERE TenDN = '$username' OR Email = '$email'";
- 
     // Thực thi câu truy vấn
     $result = mysqli_query($conn, $sql);
-    
     $tinhtrang=1;
     $quyen="User";
     $today = date("Y-m-d");
@@ -36,8 +49,11 @@ if(isset($_POST['dangky'])){
     else {
         $sql1 = "INSERT INTO `taikhoan`( `TenDN`, `MatKhau`, `Email`, `NgayTao`, `TinhTrang`, `Quyen`) VALUES ('$username','$password','$email','$today','$tinhtrang','$quyen')";
         $result1= mysqli_query($conn, $sql1);
+        $mataikhoan=getMaTaiKhoan($conn);
+        $sql2 = "INSERT INTO `khachhang`(`TenKhach`, `DiaChi`, `SDT`, `MaTaiKhoan`) VALUES ('$TenKhach','$DiaChi','$SDT','$mataikhoan[0]')";
+        $result2= mysqli_query($conn, $sql2);
         echo '<script language="javascript">alert("Ban da dang ky thanh cong!"); window.location="index.php";</script>';
-        $_SESSION['MaTaiKhoan'] = $username;
+        $_SESSION['MaTaiKhoan'] = $mataikhoan;
         die(); 
     }
 }
