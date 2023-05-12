12764a3689c445a0614f3ea512db0de97597b327
Linh1023 <116327425+Linh1023@users.noreply.github.com>
2023-05-12 15:44:26 +0700

    update

--- account/login.php
+++ account/login.php
@@ -1,19 +1,18 @@
-
-<link rel="stylesheet" href="../css/formDN.css"/>
+<link rel="stylesheet" href="../css/formDN.css" />
 <link rel="stylesheet" href="themify-icons-font/themify-icons/themify-icons.css">
 
-<form method="post" action="./index.php" id="form" >
+<form method="post" action="./index.php" id="form">
     <p onclick=tatdn()>X</p>
     <h2>Đăng nhập</h2>
-    Username: 
+    Username:
     <input type="text" name="username" value="" required />
-    Password: 
+    Password:
     <input type="password" name="password" value="" required />
-    <br/>
-    <br/>
-    <input type="submit" name="dangnhap" value="Đăng Nhập"/>
-    <br/>
-    <br/>
-    <input type="button" name="dangky" value="Đăng Ký" onclick = hienthidangky()  />
-    <?php require 'login_submit.php';?>
+    <br />
+    <br />
+    <input type="submit" name="dangnhap" value="Đăng Nhập" />
+    <br />
+    <br />
+    <input type="button" name="dangky" value="Đăng Ký" onclick=hienthidangky() />
+    <?php require 'login_submit.php'; ?>
 </form>
