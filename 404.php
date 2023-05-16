858bb46f0dc94dc2746f047faf14c29f6455f2b9
Linh1023 <116327425+Linh1023@users.noreply.github.com>
2023-05-17 00:11:17 +0700

    trang không tồn tại

---
+++ 404.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Trang không tồn tại</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            padding: 50px;
+        }
+
+        h1 {
+            font-size: 36px;
+            margin-bottom: 20px;
+        }
+
+        p {
+            font-size: 18px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Lỗi 404 - Trang không tồn tại</h1>
+    <p>Xin lỗi, trang bạn đang tìm kiếm không tồn tại.</p>
+</body>
+</html>
