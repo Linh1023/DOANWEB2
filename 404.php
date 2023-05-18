aec82595bc6ebddd239fb4e440cf1db05788ad88
trungkien2543 <trungkien1862@gmail.com>
2023-05-18 11:15:28 +0700

    Update

--- 404.php
+++ 404.php
@@ -6,21 +6,64 @@
         body {
             font-family: Arial, sans-serif;
             text-align: center;
-            padding: 50px;
+        }
+        #container {
+            margin: auto;
+            height: 600px;
+            background: gainsboro;
+            display: block;
+            width: 900px;
+            padding: 20px 0px;
         }
 
-        h1 {
-            font-size: 36px;
-            margin-bottom: 20px;
+        #content1{
+            background: deepskyblue;
+            width: 50%;
+            height: 335px;
+            display: block;
+            margin: auto;
+            padding-top: 43px;
+            border-radius: 44px;
         }
 
-        p {
-            font-size: 18px;
+        #content1 h1{
+            font-size: 240px;
+            margin-top: 0px;
+            color: white;
         }
+
+        #content2 h2{
+            margin: 35px 0px;
+            text-transform: uppercase;
+            text-align: center;
+            font-size: 30px;
+        }
+
+        #content2 p{
+            font-size: 20px;
+        }
+
+        #content2 p a{
+            text-decoration: none;
+            color:red;
+        }
+
+        #content2 p a:hover{
+            color: purple;
+        }
+
     </style>
 </head>
 <body>
-    <h1>Lỗi 404 - Trang không tồn tại</h1>
-    <p>Xin lỗi, trang bạn đang tìm kiếm không tồn tại.</p>
+    <div id = "container">
+        <div id = "content1">
+            <h1>404</h1>
+        </div>
+        <div id = "content2">
+            <h2>Trang bạn tìm kiếm không tồn tại</h2>
+            <p>Có thể URL bị hỏng hoặc chủ blog xóa bỏ</p>
+            <p>Trở về trang chủ <a href="./index.php">Giày đá bóng chính hãng</a></p>
+        </div>
+    </div>
 </body>
 </html>
