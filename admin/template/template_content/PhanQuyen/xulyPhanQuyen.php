d5108aac82335a8792a71fb0053bff1cc4f1fd98
trungkien2543 <trungkien1862@gmail.com>
2023-05-18 09:45:25 +0700

    Update

--- admin/template/template_content/PhanQuyen/xulyPhanQuyen.php
+++ admin/template/template_content/PhanQuyen/xulyPhanQuyen.php
@@ -14,7 +14,7 @@
 ?>
 <form action="" method="POST">
         <div id="check_item">
-            <a id="exit" href="../../../index.php?pq=Admin&id=pq">X</a>
+            <a id="exit" href="../../../index.php?id=pq">X</a>
             <div class="row">
                 <?php
                     $data = $db->getListCTQ();
@@ -59,7 +59,7 @@
             
         } 
 
-        echo "<script>alert('Phan quyen thanh cong');window.location='../../../index.php?pq=Admin&id=pq';</script>";
+        echo "<script>alert('Phan quyen thanh cong');window.location='../../../index.php?id=pq';</script>";
     }
 ?>
 
